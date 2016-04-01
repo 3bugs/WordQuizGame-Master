292ac33a61ae8b90f3ebd5daefdbd750a658bf49
Promlert Lovichit <promlert@gmail.com>
2016-04-02 00:17:44 +0700

    Add lots of things

---
+++ app/src/main/java/com/example/wordquizgamemaster/GameActivity.java
@@ -0,0 +1,350 @@
+package com.example.wordquizgamemaster;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.example.wordquizgamemaster.db.DatabaseHelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity {
+
+    private static final String TAG = GameActivity.class.getSimpleName();
+
+    public static final String KEY_DIFFICULTY = "difficulty";
+    private static final int NUM_QUESTIONS_PER_QUIZ = 3;
+
+    private TextView mQuestionNumberTextView;
+    private ImageView mQuestionImageView;
+    private TextView mAnswerTextView;
+    private TableLayout mButtonTableLayout;
+
+    private int mDifficulty;
+    private int mNumChoices;
+
+    private ArrayList<String> mFileNameList = new ArrayList<>();
+    private ArrayList<String> mQuizWordList = new ArrayList<>();
+    private ArrayList<String> mChoiceWordList = new ArrayList<>();
+
+    private String mAnswerFileName;
+    private int mTotalGuesses;
+    private int mScore;
+
+    private Random mRandom = new Random();
+    private Handler mHandler = new Handler();
+
+    private Animation shakeAnimation;
+
+    private DatabaseHelper mHelper;
+    private SQLiteDatabase mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        setupViews();
+
+        mHelper = new DatabaseHelper(this);
+        mDatabase = mHelper.getWritableDatabase();
+
+        Intent i = getIntent();
+        mDifficulty = i.getIntExtra(KEY_DIFFICULTY, 0);
+        switch (mDifficulty) {
+            case 0:
+                mNumChoices = 2;
+                break;
+            case 1:
+                mNumChoices = 4;
+                break;
+            case 2:
+                mNumChoices = 6;
+                break;
+        }
+
+        shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
+        shakeAnimation.setRepeatCount(3);
+
+        mHelper = new DatabaseHelper(this);
+        mDatabase = mHelper.getWritableDatabase();
+
+        getImageFileNames();
+    }
+
+    private void setupViews() {
+        mQuestionNumberTextView = (TextView) findViewById(R.id.question_number_text_view);
+        mQuestionImageView = (ImageView) findViewById(R.id.question_image_view);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mButtonTableLayout = (TableLayout) findViewById(R.id.button_table_layout);
+    }
+
+    private void getImageFileNames() {
+        String[] categories = new String[]{"animals", "body", "colors", "numbers", "objects"};
+
+        AssetManager am = getAssets();
+        for (String c : categories) {
+            try {
+                String[] fileNames = am.list(c);
+
+                for (String f : fileNames) {
+                    mFileNameList.add(f.replace(".png", ""));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Error listing file name in " + c);
+            }
+        }
+
+        Log.i(TAG, "***** รายชื่อไฟล์ภาพทั้งหมด");
+        for (String f : mFileNameList) {
+            Log.i(TAG, f);
+        }
+
+        startQuiz();
+    }
+
+    private void startQuiz() {
+        mScore = 0;
+        mTotalGuesses = 0;
+        mQuizWordList.clear();
+
+        while (mQuizWordList.size() < NUM_QUESTIONS_PER_QUIZ) {
+            int randomIndex = mRandom.nextInt(mFileNameList.size());
+            String fileName = mFileNameList.get(randomIndex);
+
+            if (mQuizWordList.contains(fileName) == false) {
+                mQuizWordList.add(fileName);
+            }
+        }
+
+        Log.i(TAG, "***** รายชื่อไฟล์คำถามที่สุ่มได้");
+        for (String f : mQuizWordList) {
+            Log.i(TAG, f);
+        }
+
+        loadNextQuestion();
+    }
+
+    private void loadNextQuestion() {
+        mAnswerTextView.setText(null);
+        mAnswerFileName = mQuizWordList.remove(0);
+
+        String msg = String.format("คำถาม %d จาก %d", mScore + 1, NUM_QUESTIONS_PER_QUIZ);
+        mQuestionNumberTextView.setText(msg);
+
+        Log.i(TAG, "***** ชื่อไฟล์รูปภาพคำถามคือ " + mAnswerFileName);
+
+        loadQuestionImage();
+        prepareChoiceWords();
+    }
+
+    private void loadQuestionImage() {
+        String category = mAnswerFileName.substring(
+                0,
+                mAnswerFileName.indexOf('-')
+        );
+
+        String filePath = category + "/" + mAnswerFileName + ".png";
+
+        AssetManager am = getAssets();
+        InputStream stream;
+
+        try {
+            stream = am.open(filePath);
+            Drawable image = Drawable.createFromStream(stream, filePath);
+            mQuestionImageView.setImageDrawable(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Error loading image file: " + filePath);
+        }
+    }
+
+    private void prepareChoiceWords() {
+        mChoiceWordList.clear();
+        String answerWord = getWord(mAnswerFileName);
+
+        while (mChoiceWordList.size() < mNumChoices) {
+            int randomIndex = mRandom.nextInt(mFileNameList.size());
+            String randomWord = getWord(mFileNameList.get(randomIndex));
+
+            if (!mChoiceWordList.contains(randomWord) && !answerWord.equals(randomWord)) {
+                mChoiceWordList.add(randomWord);
+            }
+        }
+
+        int randomIndex = mRandom.nextInt(mChoiceWordList.size());
+        mChoiceWordList.set(randomIndex, answerWord);
+
+        Log.i(TAG, "***** คำศัพท์ตัวเลือกที่สุ่มได้");
+        for (String w : mChoiceWordList) {
+            Log.i(TAG, w);
+        }
+
+        createChoiceButtons();
+    }
+
+    private String getWord(String fileName) {
+        return fileName.substring(fileName.indexOf('-') + 1);
+    }
+
+    private void createChoiceButtons() {
+        for (int row = 0; row < mButtonTableLayout.getChildCount(); row++) {
+            TableRow tr = (TableRow) mButtonTableLayout.getChildAt(row);
+            tr.removeAllViews();
+        }
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        for (int row = 0; row < mNumChoices / 2; row++) {
+            TableRow tr = (TableRow) mButtonTableLayout.getChildAt(row);
+
+            for (int column = 0; column < 2; column++) {
+                Button guessButton = (Button) inflater.inflate(R.layout.guess_button, tr, false);
+                guessButton.setText(mChoiceWordList.remove(0));
+                guessButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        submitGuess((Button) v);
+                    }
+                });
+                tr.addView(guessButton);
+            }
+        }
+    }
+
+    private void submitGuess(Button guessButton) {
+        Log.i(TAG, "You selected " + guessButton.getText().toString());
+
+        mTotalGuesses++;
+
+        String guessWord = guessButton.getText().toString();
+        String answerWord = getWord(mAnswerFileName);
+
+        // ตอบถูก
+        if (guessWord.equals(answerWord)) {
+            mScore++;
+
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.applause);
+            mp.start();
+
+            String msg = guessWord + " ถูกต้องนะคร้าบบ";
+            mAnswerTextView.setText(msg);
+            mAnswerTextView.setTextColor(
+                    ContextCompat.getColor(this, android.R.color.holo_green_dark));
+
+            disableAllButtons();
+
+            // ตอบถูก และเล่นครบทุกข้อแล้ว (จบเกม)
+            if (mScore == NUM_QUESTIONS_PER_QUIZ) {
+                saveScore();
+
+                String msgResult = String.format(
+                        "จำนวนครั้งที่ทาย: %d\nเปอร์เซ็นต์ความถูกต้อง: %.1f",
+                        mTotalGuesses,
+                        100 * NUM_QUESTIONS_PER_QUIZ / (double) mTotalGuesses
+                );
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setTitle("สรุปผล");
+                dialog.setMessage(msgResult);
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("เริ่มเกมใหม่", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startQuiz();
+                    }
+                });
+                dialog.setNegativeButton("กลับหน้าหลัก", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                dialog.show();
+            }
+            // ตอบถูก แต่ยังไม่ครบทุกข้อ (ยังไม่จบเกม)
+            else {
+                mHandler.postDelayed(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                loadNextQuestion();
+                            }
+                        }
+                        , 2000
+                );
+            }
+        }
+        // ตอบผิด
+        else {
+            guessButton.setEnabled(false);
+
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.fail);
+            mp.start();
+
+            mQuestionImageView.startAnimation(shakeAnimation);
+
+            String msg = "ผิดครับ ลองใหม่นะครับ";
+            mAnswerTextView.setText(msg);
+            mAnswerTextView.setTextColor(
+                    ContextCompat.getColor(this, android.R.color.holo_red_dark));
+        }
+    }
+
+    private void disableAllButtons() {
+        for (int row = 0; row < mButtonTableLayout.getChildCount(); row++) {
+            TableRow tr = (TableRow) mButtonTableLayout.getChildAt(row);
+
+            for (int column = 0; column < tr.getChildCount(); column++) {
+                Button b = (Button) tr.getChildAt(column);
+                b.setEnabled(false);
+            }
+        }
+    }
+
+    private void saveScore() {
+        ContentValues cv = new ContentValues();
+
+        double percentScore = 100 * NUM_QUESTIONS_PER_QUIZ / (double) mTotalGuesses;
+
+        cv.put(DatabaseHelper.COL_SCORE, percentScore);
+        cv.put(DatabaseHelper.COL_DIFFICULTY, mDifficulty);
+        mDatabase.insert(DatabaseHelper.TABLE_NAME, null, cv);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Music.play(this, R.raw.game);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Music.stop();
+    }
+}
--- app/src/main/java/com/example/wordquizgamemaster/MainActivity.java
+++ app/src/main/java/com/example/wordquizgamemaster/MainActivity.java
@@ -1,13 +1,171 @@
 package com.example.wordquizgamemaster;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mPlayGameButton;
+    private Button mHighScoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayGameButton = (Button) findViewById(R.id.play_game_button);
+        mPlayGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "Hello log");
+
+                Toast t = Toast.makeText(
+                        MainActivity.this,
+                        "Hello Toast",
+                        Toast.LENGTH_SHORT
+                );
+                t.show();
+
+                showCustomChooseDifficultyDialog();
+
+/*
+                final String[] items = new String[]{"ง่าย", "ปานกลาง", "ยาก"};
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("เลือกระดับความยาก");
+                //dialog.setMessage("Message");
+                dialog.setIcon(R.drawable.abc);
+                dialog.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "คุณเลือก: " + items[which]);
+
+                        Intent i = new Intent(MainActivity.this, GameActivity.class);
+                        i.putExtra(GameActivity.KEY_DIFFICULTY, which);
+                        startActivity(i);
+                    }
+                });
+                dialog.show();
+*/
+
+/*
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "คุณคลิกปุ่ม OK");
+                    }
+                });
+
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "คุณคลิกปุ่ม Cancel");
+                    }
+                });
+*/
+            }
+        });
+
+        mHighScoreButton = (Button) findViewById(R.id.high_score_button);
+        mHighScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HighScoreActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void showCustomChooseDifficultyDialog() {
+        AlertDialog.Builder chooseDiffDialog = new AlertDialog.Builder(MainActivity.this);
+
+        chooseDiffDialog.setTitle("เลือกระดับความยาก");
+        chooseDiffDialog.setCancelable(true);
+
+        final String[] items = new String[]{"ง่าย", "ปานกลาง", "ยาก"};
+        DifficultyOptionsAdapter adapter = new DifficultyOptionsAdapter(
+                this,
+                R.layout.difficulty_row,
+                new ArrayList<>(Arrays.asList(items))
+        );
+
+        chooseDiffDialog.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra(GameActivity.KEY_DIFFICULTY, which);
+                startActivity(intent);
+            }
+        });
+        chooseDiffDialog.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Music.play(this, R.raw.main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Music.stop();
+    }
+
+    private static class DifficultyOptionsAdapter extends ArrayAdapter<String> {
+
+        private Context context;
+        private int itemLayoutId;
+        private ArrayList<String> difficulties;
+
+        public DifficultyOptionsAdapter(Context context, int itemLayoutId,
+                                        ArrayList<String> difficulties) {
+            super(context, itemLayoutId, difficulties);
+
+            this.context = context;
+            this.itemLayoutId = itemLayoutId;
+            this.difficulties = difficulties;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = inflater.inflate(itemLayoutId, parent, false);
+
+            TextView difficultyTextView = (TextView) row.findViewById(R.id.difficultyTextView);
+            ImageView difficultyImageView = (ImageView) row.findViewById(R.id.difficultyImageView);
+
+            String diff = difficulties.get(position);
+            difficultyTextView.setText(diff);
+
+            if (diff.equals("ง่าย")) {
+                difficultyImageView.setImageResource(R.drawable.dog_easy);
+            } else if (diff.equals("ปานกลาง")) {
+                difficultyImageView.setImageResource(R.drawable.dog_medium);
+            } else if (diff.equals("ยาก")) {
+                difficultyImageView.setImageResource(R.drawable.dog_hard);
+            }
+
+            return row;
+        }
     }
 }
